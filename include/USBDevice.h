11baf973fa86542131e2f067ddc883b01f67e7ba
OleksandrDreval <oleksandr.dreval@nure.ua>
2025-08-31 12:35:38 +0300

    feat(include): Added header file USBDevice.h with functions for working with USB devices

---
+++ include/USBDevice.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <windows.h>
+
+// USB device functions
+BOOL Read(HANDLE hDevice, LPVOID lpBuffer, DWORD nNumberOfBytesToRead, LPDWORD lpNumberOfBytesRead, LPOVERLAPPED lpOverlapped);
+BOOL Write(HANDLE hDevice, LPCVOID lpBuffer, DWORD nNumberOfBytesToWrite, LPDWORD lpNumberOfBytesWritten, LPOVERLAPPED lpOverlapped);
+HANDLE InitializeUSBDevice(LPCSTR devicePath);
+void CloseUSBDevice(HANDLE hDevice);
+
+// Constants for USB operations
+#define USB_TIMEOUT 5000
+#define USB_BUFFER_SIZE 4096
+#define USB_MAX_DEVICES 10
