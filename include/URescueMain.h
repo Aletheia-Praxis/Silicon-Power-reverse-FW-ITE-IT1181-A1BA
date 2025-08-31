c8d37cad7e09757722bb4d6f42f64d374129dcf7
OleksandrDreval <oleksandr.dreval@nure.ua>
2025-08-31 12:34:34 +0300

    feat(include): Added header file URescueMain.h with declarations of main functions and class CMainFrame

---
+++ include/URescueMain.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <afxwin.h>
+
+// Main function of the URescue program
+int FUN_004845e0(void);
+
+// MFC WinMain function
+int AfxWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow);
+
+// Main window of the program
+class CMainFrame : public CFrameWnd
+{
+public:
+    CMainFrame();
+    virtual ~CMainFrame();
+    
+protected:
+    DECLARE_MESSAGE_MAP()
+};
