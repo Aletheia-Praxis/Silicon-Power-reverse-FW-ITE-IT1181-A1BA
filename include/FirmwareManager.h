7dbb2da9a6314220c7930deb09138dcf35818a2b
OleksandrDreval <oleksandr.dreval@nure.ua>
2025-08-31 14:56:33 +0300

    feat(include): Added header file FirmwareManager.h with functions for firmware management and corresponding constants

---
+++ include/FirmwareManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <windows.h>
+
+// Firmware management functions
+BOOL LoadFirmware(LPCSTR firmwarePath, LPVOID* ppBuffer, DWORD* pSize);
+BOOL WriteFirmware(HANDLE hDevice, LPCVOID pFirmware, DWORD firmwareSize);
+BOOL VerifyFirmware(HANDLE hDevice, LPCVOID pFirmware, DWORD firmwareSize);
+void CleanupFirmware(LPVOID pBuffer);
+
+// Constants for firmware management
+#define FIRMWARE_HEADER_SIZE 512
+#define FIRMWARE_SIGNATURE "ITE_FW"
+#define FIRMWARE_VERSION_SIZE 32
