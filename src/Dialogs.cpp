913f0e11f9420d0b19e93eef898b16a7ddb9b323
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2025-09-11 09:53:45 +0300

    refactor(Dialogs): Clean up includes and improve code formatting for readability

--- src/Dialogs.cpp
+++ src/Dialogs.cpp
@@ -1,13 +1,9 @@
-#include <afxwin.h>
-#include <afxcmn.h>
-#include <afxinet.h>
-#include <afxdlgs.h>
+#include "WindowsHeaders.h"
 #include "Dialogs.h"
 #include "resource.h"
 
 // Device selection dialog constructor
-CDeviceSelectDialog::CDeviceSelectDialog(CWnd* pParent)
-    : CDialog(IDD_DEVICE_SELECT, pParent)
+CDeviceSelectDialog::CDeviceSelectDialog(CWnd *pParent) : CDialog(IDD_DEVICE_SELECT, pParent)
 {
     m_selectedDevice = _T("");
 }
@@ -16,18 +12,18 @@ CDeviceSelectDialog::CDeviceSelectDialog(CWnd* pParent)
 BOOL CDeviceSelectDialog::OnInitDialog()
 {
     CDialog::OnInitDialog();
-    
+
     // Get a reference to the device list
     m_deviceList.SubclassDlgItem(IDC_DEVICE_LIST, this);
-    
+
     // Refresh the device list
     RefreshDeviceList();
-    
+
     return TRUE;
 }
 
 // Data exchange handler
-void CDeviceSelectDialog::DoDataExchange(CDataExchange* pDX)
+void CDeviceSelectDialog::DoDataExchange(CDataExchange *pDX)
 {
     CDialog::DoDataExchange(pDX);
     DDX_Control(pDX, IDC_DEVICE_LIST, m_deviceList);
@@ -37,69 +33,76 @@ void CDeviceSelectDialog::DoDataExchange(CDataExchange* pDX)
 void CDeviceSelectDialog::RefreshDeviceList()
 {
     m_deviceList.ResetContent();
-    
+
     // Search for USB devices with ITE controllers
     HDEVINFO hDevInfo = SetupDiGetClassDevsA(&GUID_DEVCLASS_USB, NULL, NULL, DIGCF_PRESENT);
-    if (hDevInfo == INVALID_HANDLE_VALUE) {
+    if (hDevInfo == INVALID_HANDLE_VALUE)
+    {
         return;
     }
-    
+
     SP_DEVINFO_DATA deviceInfoData;
     deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
-    
-    for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &deviceInfoData); i++) {
+
+    for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &deviceInfoData); i++)
+    {
         // Get the device description
         TCHAR deviceDesc[256];
-        if (SetupDiGetDeviceRegistryProperty(hDevInfo, &deviceInfoData, SPDRP_DEVICEDESC, NULL, 
-                                           (PBYTE)deviceDesc, sizeof(deviceDesc), NULL)) {
-            
+        if (SetupDiGetDeviceRegistryProperty(hDevInfo, &deviceInfoData, SPDRP_DEVICEDESC, NULL, (PBYTE)deviceDesc,
+                                             sizeof(deviceDesc), NULL))
+        {
+
             // Check if it's an ITE controller
-            if (_tcsstr(deviceDesc, _T("ITE")) || _tcsstr(deviceDesc, _T("1181"))) {
+            if (_tcsstr(deviceDesc, _T("ITE")) || _tcsstr(deviceDesc, _T("1181")))
+            {
                 // Get the device path
                 TCHAR devicePath[256];
-                if (GetDevicePath(hDevInfo, &deviceInfoData, devicePath, sizeof(devicePath))) {
+                if (GetDevicePath(hDevInfo, &deviceInfoData, devicePath, sizeof(devicePath)))
+                {
                     int index = m_deviceList.AddString(deviceDesc);
-                    m_deviceList.SetItemData(index, (DWORD_PTR)new CString(devicePath));
+                    m_deviceList.SetItemData(index, (DWORD_PTR) new CString(devicePath));
                 }
             }
         }
     }
-    
+
     SetupDiDestroyDeviceInfoList(hDevInfo);
 }
 
 // Get the device path
-BOOL CDeviceSelectDialog::GetDevicePath(HDEVINFO hDevInfo, PSP_DEVINFO_DATA pDeviceInfoData, 
-                                       LPTSTR pDevicePath, DWORD devicePathSize)
+BOOL CDeviceSelectDialog::GetDevicePath(HDEVINFO hDevInfo, PSP_DEVINFO_DATA pDeviceInfoData, LPTSTR pDevicePath,
+                                        DWORD devicePathSize)
 {
     // Get the device interface
     SP_DEVICE_INTERFACE_DATA deviceInterfaceData;
     deviceInterfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
-    
-    if (!SetupDiEnumDeviceInterfaces(hDevInfo, pDeviceInfoData, &GUID_DEVINTERFACE_USB_DEVICE, 
-                                    0, &deviceInterfaceData)) {
+
+    if (!SetupDiEnumDeviceInterfaces(hDevInfo, pDeviceInfoData, &GUID_DEVINTERFACE_USB_DEVICE, 0, &deviceInterfaceData))
+    {
         return FALSE;
     }
-    
+
     // Get the interface details
     DWORD requiredSize = 0;
     SetupDiGetDeviceInterfaceDetail(hDevInfo, &deviceInterfaceData, NULL, 0, &requiredSize, NULL);
-    
-    PSP_DEVICE_INTERFACE_DETAIL_DATA pDeviceInterfaceDetailData = 
+
+    PSP_DEVICE_INTERFACE_DETAIL_DATA pDeviceInterfaceDetailData =
         (PSP_DEVICE_INTERFACE_DETAIL_DATA)malloc(requiredSize);
-    if (!pDeviceInterfaceDetailData) {
+    if (!pDeviceInterfaceDetailData)
+    {
         return FALSE;
     }
-    
+
     pDeviceInterfaceDetailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
-    
-    if (SetupDiGetDeviceInterfaceDetail(hDevInfo, &deviceInterfaceData, pDeviceInterfaceDetailData, 
-                                       requiredSize, NULL, NULL)) {
+
+    if (SetupDiGetDeviceInterfaceDetail(hDevInfo, &deviceInterfaceData, pDeviceInterfaceDetailData, requiredSize, NULL,
+                                        NULL))
+    {
         _tcscpy_s(pDevicePath, devicePathSize, pDeviceInterfaceDetailData->DevicePath);
         free(pDeviceInterfaceDetailData);
         return TRUE;
     }
-    
+
     free(pDeviceInterfaceDetailData);
     return FALSE;
 }
@@ -108,9 +111,11 @@ BOOL CDeviceSelectDialog::GetDevicePath(HDEVINFO hDevInfo, PSP_DEVINFO_DATA pDev
 void CDeviceSelectDialog::OnDeviceListSelChange()
 {
     int selectedIndex = m_deviceList.GetCurSel();
-    if (selectedIndex != LB_ERR) {
-        CString* pDevicePath = (CString*)m_deviceList.GetItemData(selectedIndex);
-        if (pDevicePath) {
+    if (selectedIndex != LB_ERR)
+    {
+        CString *pDevicePath = (CString *)m_deviceList.GetItemData(selectedIndex);
+        if (pDevicePath)
+        {
             m_selectedDevice = *pDevicePath;
         }
     }
@@ -125,23 +130,23 @@ void CDeviceSelectDialog::OnRefresh()
 // OK button handler
 void CDeviceSelectDialog::OnOK()
 {
-    if (m_selectedDevice.IsEmpty()) {
+    if (m_selectedDevice.IsEmpty())
+    {
         MessageBox(_T("Please select a device"), _T("Warning"), MB_OK | MB_ICONWARNING);
         return;
     }
-    
+
     CDialog::OnOK();
 }
 
 // Message handler
 BEGIN_MESSAGE_MAP(CDeviceSelectDialog, CDialog)
-    ON_LBN_SELCHANGE(IDC_DEVICE_LIST, OnDeviceListSelChange)
-    ON_BN_CLICKED(IDC_REFRESH_BUTTON, OnRefresh)
+ON_LBN_SELCHANGE(IDC_DEVICE_LIST, OnDeviceListSelChange)
+ON_BN_CLICKED(IDC_REFRESH_BUTTON, OnRefresh)
 END_MESSAGE_MAP()
 
 // About dialog constructor
-CAboutDialog::CAboutDialog(CWnd* pParent)
-    : CDialog(IDD_ABOUT, pParent)
+CAboutDialog::CAboutDialog(CWnd *pParent) : CDialog(IDD_ABOUT, pParent)
 {
 }
 
@@ -149,23 +154,23 @@ CAboutDialog::CAboutDialog(CWnd* pParent)
 BOOL CAboutDialog::OnInitDialog()
 {
     CDialog::OnInitDialog();
-    
+
     // Set the about text
     CString aboutText;
     aboutText.Format(_T("URescue v81D.2.24.2\n\n")
-                    _T("ITE IT1181 Firmware Recovery Tool\n\n")
-                    _T("This is a reverse-engineered version of the original URescue utility.\n")
-                    _T("Use with caution and only on devices you own.\n\n")
-                    _T("Reverse Engineering Project\n")
-                    _T("For educational purposes only"));
-    
+                     _T("ITE IT1181 Firmware Recovery Tool\n\n")
+                     _T("This is a reverse-engineered version of the original URescue utility.\n")
+                     _T("Use with caution and only on devices you own.\n\n")
+                     _T("Reverse Engineering Project\n")
+                     _T("For educational purposes only"));
+
     SetDlgItemText(IDC_ABOUT_TEXT, aboutText);
-    
+
     return TRUE;
 }
 
 // Data exchange handler for the about dialog
-void CAboutDialog::DoDataExchange(CDataExchange* pDX)
+void CAboutDialog::DoDataExchange(CDataExchange *pDX)
 {
     CDialog::DoDataExchange(pDX);
 }
