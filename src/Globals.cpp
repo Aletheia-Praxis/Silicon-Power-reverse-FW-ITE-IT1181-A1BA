fd31446a22cbb7cb9f7d9f466bd59c6e6c2d1290
OleksandrDreval <180102593+OleksandrDreval@users.noreply.github.com>
2025-10-07 21:15:02 +0300

    feat(Globals): Add global function pointers for SDK and Flash Layer Helper operations

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -1,4 +1,10 @@
+// clang-format off
+// #include "../include/iTEUFDrs_Ghidra.h"  // Not needed - functionality moved to iTEUFDrs class
 #include "../include/Globals.h"
+#include "../include/FlashSDK.h"
+#include "../include/iTEUFDrs.h"
+#include "../include/SDKGlobals.h"  // For SDK_FUNCTION_PTR type definition
+// clang-format on
 
 // Define global variables
 char g_cmdLineArgs[128] = { 0 };
@@ -72,3 +78,152 @@ const DWORD ITE_ERROR_FLASH_FAILED = 0x00000004;
 const DWORD ITE_ERROR_VERIFY_FAILED = 0x00000005;
 const DWORD ITE_ERROR_TIMEOUT = 0x00000006;
 const DWORD ITE_ERROR_UNKNOWN = 0xFFFFFFFF;
+
+// Global pointer to the main iTEUFDrs object instance
+void* g_iTEUFDrs = nullptr;
+
+// Global variables for SDK function pointers (for LoadSDKFunctions)
+SDK_FUNCTION_PTR g_pSTD_Read_ID_New = NULL;
+SDK_FUNCTION_PTR g_pSTD_Read_ID = NULL;
+
+// Flash Layer Helper (FLH) function pointers
+SDK_FUNCTION_PTR g_pFLH_GetInfoFromDataBaseByID = NULL;
+SDK_FUNCTION_PTR g_pFLH_GetFlashDataFromDataBase = NULL;
+SDK_FUNCTION_PTR g_pFLH_GetFlashDataFromMemory = NULL;
+SDK_FUNCTION_PTR g_pFLH_ReadRootTable = NULL;
+SDK_FUNCTION_PTR g_pFLH_WriteRootTable = NULL;
+SDK_FUNCTION_PTR g_pFLH_ReadCISTable = NULL;
+SDK_FUNCTION_PTR g_pFLH_WriteCISTable = NULL;
+SDK_FUNCTION_PTR g_pFLH_ReadISPData = NULL;
+SDK_FUNCTION_PTR g_pFLH_WriteISPData = NULL;
+SDK_FUNCTION_PTR g_pFLH_ReadLatestWBT = NULL;
+SDK_FUNCTION_PTR g_pFLH_FindRootTable = NULL;
+SDK_FUNCTION_PTR g_pFLH_LBA2PhysicalFlash = NULL;
+SDK_FUNCTION_PTR g_pFLH_SetLedBlink = NULL;
+SDK_FUNCTION_PTR g_pFLH_PhyiscalRead = NULL;
+SDK_FUNCTION_PTR g_pFLH_PhyiscalWrite = NULL;
+SDK_FUNCTION_PTR g_pFLH_IsGoodBlock = NULL;
+SDK_FUNCTION_PTR g_pFLH_IsTableBlock = NULL;
+SDK_FUNCTION_PTR g_pFLH_MarkBad = NULL;
+SDK_FUNCTION_PTR g_pFLH_GetRealBlocksPerDie = NULL;
+SDK_FUNCTION_PTR g_pFLH_BlockIsGap = NULL;
+SDK_FUNCTION_PTR g_pFLH_HandleMassBlocksPerChip = NULL;
+SDK_FUNCTION_PTR g_pFLH_ScanNewBlock = NULL;
+SDK_FUNCTION_PTR g_pFLH_GetRetryRegister = NULL;
+SDK_FUNCTION_PTR g_pFLH_CISCheckSum_Calculate = NULL;
+SDK_FUNCTION_PTR g_pFLH_CalCulate_ECCNO = NULL;
+SDK_FUNCTION_PTR g_pFLH_ArrangeSegmentPara = NULL;
+SDK_FUNCTION_PTR g_pFLH_ReadSpare = NULL;
+SDK_FUNCTION_PTR g_pFLH_ReadID = NULL;
+SDK_FUNCTION_PTR g_pFLH_BlockErase = NULL;
+SDK_FUNCTION_PTR g_pFLH_SetSLCFlag = NULL;
+SDK_FUNCTION_PTR g_pFLH_CPUReset = NULL;
+SDK_FUNCTION_PTR g_pFLH_InitCTRL = NULL;
+
+// Vendor Driver (VDR) function pointers
+SDK_FUNCTION_PTR g_pVDR_ReadWriteLUNConfig = NULL;
+SDK_FUNCTION_PTR g_pVDR_ReadLUNData = NULL;
+SDK_FUNCTION_PTR g_pVDR_WriteLUNData = NULL;
+SDK_FUNCTION_PTR g_pVDR_ReadXData = NULL;
+SDK_FUNCTION_PTR g_pVDR_WriteXData = NULL;
+SDK_FUNCTION_PTR g_pVDR_ReadIData = NULL;
+SDK_FUNCTION_PTR g_pVDR_WriteIData = NULL;
+SDK_FUNCTION_PTR g_pVDR_ReadSysAddr = NULL;
+SDK_FUNCTION_PTR g_pVDR_WriteSysAddr = NULL;
+SDK_FUNCTION_PTR g_pVDR_SetSYSReady = NULL;
+SDK_FUNCTION_PTR g_pVDR_EndCode = NULL;
+SDK_FUNCTION_PTR g_pVDR_DeviceChange = NULL;
+SDK_FUNCTION_PTR g_pVDR_MediaChange = NULL;
+SDK_FUNCTION_PTR g_pVDR_WriteProtect = NULL;
+SDK_FUNCTION_PTR g_pVDR_RWCurrentLUNType = NULL;
+SDK_FUNCTION_PTR g_pVDR_HiddenArea = NULL;
+SDK_FUNCTION_PTR g_pVDR_ReadWriteLUNNo = NULL;
+SDK_FUNCTION_PTR g_pVDR_FlushCache = NULL;
+SDK_FUNCTION_PTR g_pVDR_ReadPage = NULL;
+SDK_FUNCTION_PTR g_pVDR_WritePage = NULL;
+SDK_FUNCTION_PTR g_pVDR_WriteBlock_TLC = NULL;
+SDK_FUNCTION_PTR g_pVDR_GetSecurityStatus = NULL;
+SDK_FUNCTION_PTR g_pVDR_ED3PageRead = NULL;
+SDK_FUNCTION_PTR g_pVDR_BadTFindRead = NULL;
+SDK_FUNCTION_PTR g_pVDR_Enhance_SLC_Program = NULL;
+SDK_FUNCTION_PTR g_pVDR_Disable_SLC_Program = NULL;
+SDK_FUNCTION_PTR g_pVDR_MassBlocksProcess = NULL;
+SDK_FUNCTION_PTR g_pVDR_F_RST = NULL;
+SDK_FUNCTION_PTR g_pVDR_RootFunc = NULL;
+SDK_FUNCTION_PTR g_pVDR_RootPageWrite = NULL;
+SDK_FUNCTION_PTR g_pVDR_RootAccess = NULL;
+
+// Additional STD function pointers (used with VDR functions)
+SDK_FUNCTION_PTR g_pSTD_GetDeviceID = NULL;
+SDK_FUNCTION_PTR g_pSTD_SetDeviceID = NULL;
+SDK_FUNCTION_PTR g_pSTD_GetLUNIndex = NULL;
+
+// Security (SEC) function pointers
+SDK_FUNCTION_PTR g_pSEC_DoAuthentication = NULL;
+SDK_FUNCTION_PTR g_pSEC_LeaveAuthenticatedState = NULL;
+SDK_FUNCTION_PTR g_pSEC_GetPasswordHint = NULL;
+SDK_FUNCTION_PTR g_pSEC_SetPasswordHint = NULL;
+SDK_FUNCTION_PTR g_pSEC_ChangePassword = NULL;
+SDK_FUNCTION_PTR g_pSEC_GetUserPassword = NULL;
+SDK_FUNCTION_PTR g_pSEC_GetEncryptedPassword = NULL;
+
+// LUN function pointers
+SDK_FUNCTION_PTR g_pLUN_CreateLun = NULL;
+SDK_FUNCTION_PTR g_pLUN_FindLunStartLBAByItemID = NULL;
+SDK_FUNCTION_PTR g_pLUN_CreateApLunNewItemID = NULL;
+SDK_FUNCTION_PTR g_pLUN_WriteBadBlockMapToApLun = NULL;
+SDK_FUNCTION_PTR g_pLUN_ReadBadBlockMapFromApLun = NULL;
+SDK_FUNCTION_PTR g_pLUN_FindOptimumOffsetCap = NULL;
+SDK_FUNCTION_PTR g_pLUN_CalIsoSize = NULL;
+
+// Format (FMT) function pointers
+SDK_FUNCTION_PTR g_pFMT_Format = NULL;
+SDK_FUNCTION_PTR g_pFMT_GetOptimumCapacity = NULL;
+SDK_FUNCTION_PTR g_pFMT_GetOptimumLunConfig = NULL;
+SDK_FUNCTION_PTR g_pFMT_GetOSCapacity = NULL;
+
+// Standard (STD) function pointers
+SDK_FUNCTION_PTR g_pSTD_Inquiry = NULL;
+SDK_FUNCTION_PTR g_pSTD_ReadCapacity = NULL;
+SDK_FUNCTION_PTR g_pSTD_LogicalRead = NULL;
+SDK_FUNCTION_PTR g_pSTD_LogicalWrite = NULL;
+SDK_FUNCTION_PTR g_pSTD_TestUnitReady = NULL;
+
+// Utility function pointers
+SDK_FUNCTION_PTR g_pSwapDWORD = NULL;
+SDK_FUNCTION_PTR g_pSwapWORD = NULL;
+SDK_FUNCTION_PTR g_pCCBAddress2RawAddress = NULL;
+SDK_FUNCTION_PTR g_pRawAddress2CCBAddress = NULL;
+SDK_FUNCTION_PTR g_pCCBAddress2ED3Address = NULL;
+SDK_FUNCTION_PTR g_pED3Address2CCBAddress = NULL;
+SDK_FUNCTION_PTR g_pBlkAddr2RawAddr = NULL;
+SDK_FUNCTION_PTR g_pIs168Device = NULL;
+SDK_FUNCTION_PTR g_pGetLastestPage = NULL;
+SDK_FUNCTION_PTR g_pRawAddr2BlkAddr = NULL;
+SDK_FUNCTION_PTR g_pGetLastestBlockPerChannel = NULL;
+SDK_FUNCTION_PTR g_pGetECCLevelFromFlashDBID = NULL;
+SDK_FUNCTION_PTR g_pIs3DTLC = NULL;
+SDK_FUNCTION_PTR g_pReadRetry = NULL;
+SDK_FUNCTION_PTR g_pWriteRetry = NULL;
+
+// Additional FLH function pointers (continued)
+SDK_FUNCTION_PTR g_pFLH_WriteRootTableWithIspPath = NULL;
+SDK_FUNCTION_PTR g_pFLH_ScanE2NANDBlockPerChip = NULL;
+SDK_FUNCTION_PTR g_pFLH_ReadBCM = NULL;
+SDK_FUNCTION_PTR g_pFLH_InitCodeWithIspPath = NULL;
+SDK_FUNCTION_PTR g_pFLH_GetChannelCeNoAndMap = NULL;
+SDK_FUNCTION_PTR g_pFLH_InitCodeForReady = NULL;
+
+// Mass Production (MP) function pointers
+SDK_FUNCTION_PTR g_pMP_CreateSystem = NULL;
+SDK_FUNCTION_PTR g_pMP_EraseSystemTable = NULL;
+
+// Diagnostic (DG) function pointers
+SDK_FUNCTION_PTR g_pDG_GetBlockPageMapFromFlash = NULL;
+SDK_FUNCTION_PTR g_pDG_SearchReadBadTBlk = NULL;
+SDK_FUNCTION_PTR g_pDG_CalBlkRequire = NULL;
+
+// Address (ADDR) function pointers
+SDK_FUNCTION_PTR g_pADDR_ReadRootTable = NULL;
+SDK_FUNCTION_PTR g_pADDR_ReadISPData = NULL;
+SDK_FUNCTION_PTR g_pADDR_ReadCISData = NULL;
